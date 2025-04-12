ea12b37236efb197d3dbee78cab3e87984d188d6
YuraBandera <romansheretko2@gmail.com>
2025-04-12 16:56:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+document.getElementById("theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+});
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Анімований сайт Міської Ради Дніпра завантажено.");
+document.getElementById("searchInput").addEventListener("input", function () {
+    const val = this.value.toLowerCase();
+    const result = document.getElementById("searchResult");
+    if (val.includes("рада")) {
+        result.textContent = "Знайдено: Міська Рада Дніпра";
+    } else if (val.includes("місто")) {
+        result.textContent = "Знайдено: Про місто Дніпро";
+    } else {
+        result.textContent = "Нічого не знайдено";
+    }
 });
